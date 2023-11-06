8cd10fb49e69a715600eb30e9f1277d4dfd5b40a
ArchPJ <washubrnfg85@gmail.com>
2023-11-06 22:08:13 +0300

    technical commit - SecurityConfig decommented

--- src/main/java/org/linksharing/server/auth/SecurityConfig.java
+++ src/main/java/org/linksharing/server/auth/SecurityConfig.java
@@ -1,30 +1,30 @@
-//package org.linksharing.server.auth;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf()
-//                .disable()
-//                .authorizeRequests()
-//                .requestMatchers(antMatcher("/registration_page")).permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login_page").permitAll()
-//                .and()
-//                .logout().permitAll();
-//        return http.build();
-//    }
-//}
+package org.linksharing.server.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .requestMatchers(antMatcher("/registration_page")).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login_page").permitAll()
+                .and()
+                .logout().permitAll();
+        return http.build();
+    }
+}
