00c6f8e8c737e47708abc7fcba89366f048eaa6b
ArchPJ <washubrnfg85@gmail.com>
2023-11-07 22:56:44 +0300

    ok, know SecurityFilterChain works properly

--- src/main/java/org/linksharing/server/auth/SecurityConfig.java
+++ src/main/java/org/linksharing/server/auth/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
@@ -15,14 +16,13 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                .disable()
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeRequests()
-                .requestMatchers(antMatcher("/registration_page")).permitAll()
+                .requestMatchers(antMatcher("/registration_page.html")).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login_page").permitAll()
+                .loginPage("/login_page.html").permitAll()
                 .and()
                 .logout().permitAll();
         return http.build();
