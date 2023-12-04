516ba2930c10a0d24a24b67397ab0c076f087d63
ArchPJ <washubrnfg85@gmail.com>
2023-12-04 15:04:14 +0300

    Bootstrap styles for registration_page accomplished, but the spting validation crashed. Commit to check only styles.

--- src/main/java/org/linksharing/server/auth/AuthController.java
+++ src/main/java/org/linksharing/server/auth/AuthController.java
@@ -6,10 +6,13 @@ import org.linksharing.server.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class AuthController {
@@ -34,10 +37,37 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public String registerUser(@Valid User user, BindingResult result) {
+    public String registerUser(@Valid User user, BindingResult result,
+                               @RequestParam Map<String, String> incomingParams) {
 
-        if (result.hasErrors()) {
-            return "registration_page.html";
+        boolean formHasErrors = false;
+
+        String[] allParams = new String[3];
+        allParams[0] = incomingParams.get("username");
+        allParams[1] = incomingParams.get("email");
+        allParams[2] = incomingParams.get("password");
+
+        StringBuilder urlTemplate = new StringBuilder("redirect:/register?");
+
+        for (int i = 0; i < allParams.length; i++) {
+            if (!allParams[i].isBlank() && i < allParams.length - 1) {
+                int index = i;
+                urlTemplate.append(incomingParams.entrySet().stream().
+                        filter(c -> c.getValue().equals(allParams[index])).
+                        toList().get(0).
+                        getKey()).append("=").append(allParams[i]).append("&");
+            } else if (!allParams[i].isBlank() && i == allParams.length - 1) {
+                urlTemplate.append("passwordIsOk");
+            } else if (allParams[i].isBlank() && i == allParams.length - 1) {
+                urlTemplate.append("passwordIsNotOk");
+            } else {
+                formHasErrors = true;
+                urlTemplate.append("hasErrors&");
+            }
+        }
+
+        if (formHasErrors) {
+            return urlTemplate.toString();
         }
 
         String pass = user.getPassword();
