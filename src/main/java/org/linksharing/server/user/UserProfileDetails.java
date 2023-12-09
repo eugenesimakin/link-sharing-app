3792900e6a03b8b58ba9210637e7869c5c58b3ef
ArchPJ <washubrnfg85@gmail.com>
2023-12-10 00:04:51 +0300

    First commit to determine main idea and set base for farther improvements.

--- src/main/java/org/linksharing/server/user/UserProfileDetails.java
+++ src/main/java/org/linksharing/server/user/UserProfileDetails.java
@@ -1,11 +1,30 @@
 package org.linksharing.server.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "user_profiles")
 public class UserProfileDetails {
-    private String profilePicture;
+
+    @Id
+    @Column(name = "id", nullable = false, unique = true)
+    private int userId;
+
+    @Column(name = "image")
+    private String profileImage;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "links")
+    private String links;
 }
