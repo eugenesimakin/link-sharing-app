9ca8ef973ef1f446a8bd0ba7667918b18659ab33
Eugene Simakin <simakin.eugene@gmail.com>
2023-12-24 22:26:09 +0300

    Add selenium tests for the profile details page

--- src/test/java/org/linksharing/server/AppTests.java
+++ src/test/java/org/linksharing/server/AppTests.java
@@ -1,13 +1,84 @@
 package org.linksharing.server;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.linksharing.server.pages.LoginPage;
+import org.linksharing.server.pages.ProfileDetailsPage;
+import org.linksharing.server.pages.RegistrationPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AppTests {
 
-    @Test
-    void contextLoads() {
+    static SeleniumConfig sConfig;
+    @LocalServerPort
+    int port;
+
+    @BeforeAll
+    public static void beforeAll() {
+        AppTests.sConfig = new SeleniumConfig();
     }
 
+    @AfterAll
+    public static void afterAll() {
+        AppTests.sConfig.getDriver().close();
+    }
+
+    @Test
+    void basicScenario() throws InterruptedException {
+        WebDriver driver = sConfig.getDriver();
+
+        driver.get(getUrl(port) + "/register");
+        new RegistrationPage(driver)
+                .withUsername("test")
+                .withEmail("test@mail.com")
+                .withPassword("123")
+                .submit();
+
+        Thread.sleep(500); // TODO: test is unstable unless
+
+        driver.get(getUrl(port) + "/login");
+        new LoginPage(driver)
+                .withEmail("test@mail.com")
+                .withPassword("123")
+                .submit();
+
+        Duration timeout = driver.manage().timeouts().getImplicitWaitTimeout();
+        new WebDriverWait(driver, timeout).until(d -> d.findElement(By.tagName("nav")));
+
+        driver.get(getUrl(port) + "/profile");
+        assertThat(driver.getCurrentUrl()).isEqualTo(getUrl(port) + "/profile");
+
+        ProfileDetailsPage profileDetailsPage = new ProfileDetailsPage(driver)
+                .withFirstName("John")
+                .withLastName("Doe")
+                .withEmail("johndoe@mail.com");
+
+        // without scrolling - error on save btn click
+        ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        Thread.sleep(500);
+
+        profileDetailsPage.clickSave();
+
+        Thread.sleep(500);
+        driver.navigate().refresh();
+
+        profileDetailsPage.verifyFirstName("John");
+        profileDetailsPage.withLastName("Doe");
+        profileDetailsPage.verifyEmail("johndoe@mail.com");
+    }
+
+    private String getUrl(int port) {
+        return "http://localhost:" + port;
+    }
 }
