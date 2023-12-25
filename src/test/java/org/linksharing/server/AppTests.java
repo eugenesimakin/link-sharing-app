7b341b014d96b6e574bcf27169157624b47b9532
Eugene Simakin <simakin.eugene@gmail.com>
2023-12-25 23:05:11 +0300

    Add tests for Profile Details Links page (disabled until implemented)

--- src/test/java/org/linksharing/server/AppTests.java
+++ src/test/java/org/linksharing/server/AppTests.java
@@ -1,11 +1,10 @@
 package org.linksharing.server;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.linksharing.server.pages.LoginPage;
 import org.linksharing.server.pages.ProfileDetailsPage;
+import org.linksharing.server.pages.ProfileLinksPage;
 import org.linksharing.server.pages.RegistrationPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -31,23 +30,25 @@ class AppTests {
     private int port;
 
     @BeforeAll
-    public static void beforeAll() {
+    static void beforeAll() {
         AppTests.sConfig = new SeleniumConfig();
     }
 
     @AfterAll
-    public static void afterAll() {
+    static void afterAll() {
         AppTests.sConfig.getDriver().close();
     }
 
-    @Test
-    void basicScenario() throws InterruptedException {
+    @BeforeEach
+    void beforeEach() throws InterruptedException {
         WebDriver driver = sConfig.getDriver();
+
+        String username = RandomUsernameGenerator.get();
 
         driver.get(getUrl(port) + "/register");
         new RegistrationPage(driver)
-                .withUsername("test")
-                .withEmail("test@mail.com")
+                .withUsername(username)
+                .withEmail(username + "@mail.com")
                 .withPassword("123")
                 .submit();
 
@@ -56,17 +57,22 @@ class AppTests {
 
         driver.get(getUrl(port) + "/login");
         new LoginPage(driver)
-                .withEmail("test@mail.com")
+                .withEmail(username + "@mail.com")
                 .withPassword("123")
                 .submit();
 
         Duration timeout = driver.manage().timeouts().getImplicitWaitTimeout();
         new WebDriverWait(driver, timeout).until(d -> d.findElement(By.tagName("nav")));
+    }
+
+    @Test
+    void profileDetailsWithoutPicture() throws InterruptedException {
+        WebDriver driver = sConfig.getDriver();
 
         driver.get(getUrl(port) + "/profile");
         assertThat(driver.getCurrentUrl()).isEqualTo(getUrl(port) + "/profile");
 
-        ProfileDetailsPage profileDetailsPage = new ProfileDetailsPage(driver)
+        var page = new ProfileDetailsPage(driver)
                 .withFirstName("John")
                 .withLastName("Doe")
                 .withEmail("johndoe@mail.com");
@@ -75,14 +81,37 @@ class AppTests {
         ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
         Thread.sleep(DEFAULT_SLEEP_TIMEOUT);
 
-        profileDetailsPage.clickSave();
+        page.clickSave();
 
         Thread.sleep(DEFAULT_SLEEP_TIMEOUT);
         driver.navigate().refresh();
 
-        profileDetailsPage.verifyFirstName("John");
-        profileDetailsPage.verifyLastName("Doe");
-        profileDetailsPage.verifyEmail("johndoe@mail.com");
+        page.verifyFirstName("John");
+        page.verifyLastName("Doe");
+        page.verifyEmail("johndoe@mail.com");
+    }
+
+    @Test
+    @Disabled("Disabled until links functionality implemented")
+    void addRemoveSaveLinks() throws InterruptedException {
+        WebDriver driver = sConfig.getDriver();
+
+        driver.get(getUrl(port) + "/links");
+        assertThat(driver.getCurrentUrl()).isEqualTo(getUrl(port) + "/links");
+
+        var page = new ProfileLinksPage(driver)
+                .addLink("Some Title", "https://example.com")
+                .addLink("Some Title", "https://example.com");
+
+        ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        Thread.sleep(DEFAULT_SLEEP_TIMEOUT);
+
+        page.saveLinks();
+
+        Thread.sleep(DEFAULT_SLEEP_TIMEOUT);
+        driver.navigate().refresh();
+
+        page.verifyLinks();
     }
 
     private String getUrl(int port) {
