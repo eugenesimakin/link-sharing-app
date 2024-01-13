0d0417a8f78c82e8987efe29950ddce05a94cd67
ArchPJ <washubrnfg85@gmail.com>
2024-01-13 13:47:18 +0300

    Changes in public_page discarded. Corresponding logic added to UserAccount controller.

--- src/main/java/org/linksharing/server/http/UserAccountController.java
+++ src/main/java/org/linksharing/server/http/UserAccountController.java
@@ -1,10 +1,22 @@
 package org.linksharing.server.http;
 
+import org.linksharing.server.user.UserProfileDetails;
+import org.linksharing.server.user.UserProfileDetailsRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class UserAccountController {
+
+    private final UserProfileDetailsRepository userProfileDetailsRepository;
+
+    public UserAccountController(UserProfileDetailsRepository userProfileDetailsRepository) {
+        this.userProfileDetailsRepository = userProfileDetailsRepository;
+    }
+
     @GetMapping("/")
     public String homePage() {
         return "redirect:/links";
@@ -21,7 +33,11 @@ public class UserAccountController {
     }
 
     @GetMapping("/public")
-    public String userPublicPage() {
+    public String userPublicPage(Principal user, Model model) {
+
+        UserProfileDetails details = userProfileDetailsRepository.findByEmail(user.getName());
+        model.addAttribute("details", details);
+
         return "user's_public_page.html";
     }
 
