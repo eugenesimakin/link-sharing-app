269886396907f9ed18ccc2f83578f88de0d4b6d6
ArchPJ <32965556+Washubrnfg85@users.noreply.github.com>
2024-02-11 19:46:08 +0300

    Implement public page functionality (#19)
    * Draft of public page and functionality implemented
    
    * UserAccountController and public_page modified. Profile picture functionality works properly.
    
    * Add public link on public page
    
    * SecurityConfig fixed in order to maintain main functionality.
    
    * Public page and UserAccountController fixed in order to maintain main functionality.
    
    * Public view optimised for publicity.

--- src/main/java/org/linksharing/server/http/UserAccountController.java
+++ src/main/java/org/linksharing/server/http/UserAccountController.java
@@ -1,10 +1,33 @@
 package org.linksharing.server.http;
 
+import org.linksharing.server.user.User;
+import org.linksharing.server.user.UserProfileDetails;
+import org.linksharing.server.user.UserProfileDetailsRepository;
+import org.linksharing.server.user.UserRepository;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.PathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
+
+import static org.springframework.http.MediaType.IMAGE_JPEG;
 
 @Controller
 public class UserAccountController {
+
+    private final UserProfileDetailsRepository profileRepository;
+    private final UserRepository userRepository;
+
+    public UserAccountController(UserProfileDetailsRepository profileRepository, UserRepository userRepository) {
+        this.profileRepository = profileRepository;
+        this.userRepository = userRepository;
+    }
+
     @GetMapping("/")
     public String homePage() {
         return "redirect:/links";
@@ -18,6 +41,58 @@ public class UserAccountController {
     @GetMapping("/profile")
     public String profileDetailsPage() {
         return "profile_details_page.html";
+    }
+
+    @GetMapping("/public")
+    public ModelAndView personalPublicPage(Principal principalUser) {
+
+        UserProfileDetails details = profileRepository.findByEmail(principalUser.getName());
+        User user = userRepository.findByEmail(principalUser.getName());
+
+        ModelAndView modelAndView = new ModelAndView("public_page");
+        modelAndView.addObject("details", details);
+        modelAndView.addObject("username", user.getUsername());
+        modelAndView.addObject("owner", "owner");
+
+        return modelAndView;
+    }
+
+    @GetMapping("/public/{username}")
+    public ModelAndView userPublicPage(@PathVariable(value="username") final String username) {
+
+        ModelAndView modelAndView = new ModelAndView("public_page");
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            modelAndView.addObject("notFound", "notNull");
+            return modelAndView;
+        }
+
+        UserProfileDetails details = profileRepository.findByEmail(user.getEmail());
+
+        modelAndView.addObject("details", details);
+        modelAndView.addObject("username", user.getUsername());
+        modelAndView.addObject("owner", null);
+
+        return modelAndView;
+    }
+
+    @GetMapping("/public/picture/{email}")
+    ResponseEntity<Resource> getProfilePicture(@PathVariable(value="email") final String email) {
+
+        UserProfileDetails userProfile = profileRepository.findByEmail(email);
+        Resource imgResource;
+
+        if (userProfile.getImageUrl() != null) {
+            imgResource = new PathResource(userProfile.getImageUrl());
+        } else {
+            imgResource = new ClassPathResource("static/placeholder.jpg");
+        }
+
+        return ResponseEntity
+                .ok()
+                .contentType(IMAGE_JPEG)
+                .body(imgResource);
     }
 
 }
--- src/main/java/org/linksharing/server/user/UserRepository.java
+++ src/main/java/org/linksharing/server/user/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends CrudRepository<User, String> {
     User findByEmail(String email);
+    User findByUsername(String username);
     boolean existsByEmail(String email);
     boolean existsByUsername(String username);
 }
